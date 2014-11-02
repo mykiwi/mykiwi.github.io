d1c86de46642698fd4297b857d52a1db9b37bf09
Romain Gautier <mail@romain.sh>
2014-11-02 23:03:39 +0100

    added base

---
+++ src/AppBundle/Controller/www/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller\www;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('www/index.html.twig');
+    }
+}
