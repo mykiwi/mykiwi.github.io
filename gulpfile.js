a1af3803dc608baa0d9217f9a1a8aa9507794a21
Romain Gautier <mail@romain.sh>
2014-11-09 00:41:47 +0100

    improved gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,55 @@
-var gulp       = require('gulp')
-    jshint     = require('gulp-jshint'),
+var gulp       = require('gulp'),
     concat     = require('gulp-concat'),
     uglify     = require('gulp-uglify'),
-    rename     = require('gulp-rename'),
-    livereload = require('gulp-livereload'),
-    watch      = require('gulp-watch');
+    // imagemin   = require('gulp-imagemin'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass       = require('gulp-sass'),
+    del        = require('del'),
+    livereload = require('gulp-livereload');
 
-var src = 'app/Resources/public/';
-var dst = 'web/';
+var paths = {
+    stylesheets : 'app/Resources/public/scss/style.scss',
+    javascripts : 'app/Resources/public/js/**/*.js'//,
+    // images      : 'app/Resources/public/img/**/*'
+};
 
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src(root + 'js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+gulp.task('clean', function(cb) {
+  del(['web/build'], cb);
 });
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src(root + 'js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+gulp.task('css', ['clean'], function() {
+  return gulp.src(paths.stylesheets)
+    .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(concat('all.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('web/build/css'))
+    .pipe(livereload());
 });
 
-gulp.task('default', function () {
-
+gulp.task('js', ['clean'], function() {
+  return gulp.src(paths.javascripts)
+    .pipe(sourcemaps.init())
+      .pipe(concat('all.js'))
+      .pipe(uglify())
+      .pipe(concat('all.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('web/build/js'))
+    .pipe(livereload());
 });
+
+// gulp.task('img', ['clean'], function() {
+//   return gulp.src(paths.images)
+//     .pipe(imagemin({optimizationLevel: 5}))
+//     .pipe(gulp.dest('web/build/img'))
+//     .pipe(livereload());
+// });
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(paths.stylesheets, ['css']).on('change', livereload.changed);;
+  gulp.watch(paths.javascripts, ['js']).on('change', livereload.changed);;
+  // gulp.watch(paths.images,      ['img']);
+});
+
+gulp.task('default', ['js', 'css']); //, 'img']);
